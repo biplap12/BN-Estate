f642f014676842648df9b3883b90e2aa7fa4ff4b
biplap <biplap.neupane2020@gmail.com>
2024-04-04 14:22:00 +0545

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000' || 'https://bn-estate.onrender.com', 
+        target: 'http://localhost:3000', 
         secure: false,
       },
     },
